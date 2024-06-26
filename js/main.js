e00d4ec5acaf22f71bcc8eb12282ada29d8c6dd2
Alexander <alexandergonzalezsaavedra@gmail.com>
2024-06-26 09:06:15 -0500

    elimine comentarios y espacios en blanco

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
-/*! For license information please see main.c1abed31.js.LICENSE.txt */
 if (window.innerWidth <= 768) {
-    const navbarText = document.querySelector(".collapse");
-    document.addEventListener("click", (e) => {
-        if (e.target.matches(".nav-item .nav-link")) {
-            navbarText.classList.remove("show");
-        }
-        if (e.target.matches(".accesos-principales-movil a")) {
-            navbarText.classList.remove("show");
-        }
+    const navbarText = document.querySelector(".collapse"); document.addEventListener("click", (e) => {
+        if (e.target.matches(".nav-item .nav-link")) { navbarText.classList.remove("show"); }
+        if (e.target.matches(".accesos-principales-movil a")) { navbarText.classList.remove("show"); }
     });
-} else {
-    console.log("mayor a 768px");
-}
+} else { console.log("mayor a 768px"); }
